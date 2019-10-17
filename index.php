a0afd7fc29e2cfbcd6a4c593e806cc053eff2f06
Adrien THO <adrien.tho@icof.fr>
2019-10-17 15:43:22 +0200

    test3

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 	<link rel="stylesheet" href="skin/maquette.css" media="screen" />
 	<link rel="stylesheet" href="skin/maquetteCatalogue.css" media="screen" />
 	<!--<link rel="stylesheet" href="skin/maquette2.css" media="screen" />-->
+	<!--LUCAS LPB-->
 	<!-- <script src="script.js"></script> -->
 </head>
 
